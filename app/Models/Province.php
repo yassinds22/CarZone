1dfecf0e10888c78f1f27c4ae5f326f636e9a646
yassinds22 <ttm126188@gmail.com>
2025-10-13 13:07:22 +0300

    add tables brands and province

---
+++ app/Models/Province.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Province extends Model
+{
+        use HasFactory;
+
+    protected $fillable = ['name', 'parent_id'];
+
+    /**
+     * العلاقة مع المحافظة الأم
+     */
+    public function parent()
+    {
+        return $this->belongsTo(Province::class, 'parent_id');
+    }
+
+    /**
+     * العلاقة مع المحافظات الفرعية
+     */
+    public function children()
+    {
+        return $this->hasMany(Province::class, 'parent_id');
+    }
+    //
+}
